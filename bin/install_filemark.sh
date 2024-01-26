bc2f1ef3e2c4fc6dcba305e735151089a4e0dac3
Shreyas-Ashtamkar <shreyu@programmer.net>
2024-01-26 13:51:21 +0530

    Fix : Linux InstallScript

--- bin/install_filemark.sh
+++ bin/install_filemark.sh
@@ -3,9 +3,27 @@
 # Define variables
 pythonScriptPath="$PWD/../filemark.py"
 scriptPath="./filemark.sh"
+DATABASE_PATH="$PWD/../silver-guide.db"
+
+# Get the default shell of the user from /etc/passwd
+curr_shell="$(getent passwd "$USER" | cut -d: -f7)"
 
 # Define function to uninstall
 uninstall() {
+    if [[ $curr_shell == *"bash"* ]]; then
+        # Bash
+        profilePath="$HOME/.bashrc"
+    elif [[ $curr_shell == *"zsh"* ]]; then
+        # Zsh
+        profilePath="$HOME/.zshrc"
+    elif [[ $curr_shell == *"fish"* ]]; then
+        # Fish
+        profilePath="$HOME/.config/fish/config.fish"
+    else
+        echo "Error: Unsupported shell. Please configure 'filemark' alias manually."
+        exit 1
+    fi
+
     echo "Uninstalling..."
     rm -f "$scriptPath" # Remove the script file
     sed -i '/# FILEMARK START/,/# FILEMARK END/d' "$profilePath" # Remove the added lines from the profile file
@@ -18,31 +36,26 @@ if [[ "$1" == "--uninstall" ]]; then
     uninstall
 fi
 
-# Get the default shell of the user from /etc/passwd
-curr_shell="$(getent passwd "$USER" | cut -d: -f7)"
-
 # Check the current shell
 if [[ $curr_shell == *"bash"* ]]; then
     # Bash
     profilePath="$HOME/.bashrc"
     echo "# FILEMARK START" > "$scriptPath"
-    echo "#!$(which bash)" >> "$scriptPath"
-    echo "alias filemark=\"python3 \\\"$pythonScriptPath\\\" \\\"\$@\\\"\"" >> "$scriptPath"
+    echo "alias filemark=\"DATABASE_PATH=\\\"$DATABASE_PATH\\\" python3 \\\"$pythonScriptPath\\\" \\\"\$@\\\"\"" >> "$scriptPath"
     echo "# FILEMARK END" >> "$scriptPath"
 elif [[ $curr_shell == *"zsh"* ]]; then
     # Zsh
     profilePath="$HOME/.zshrc"
     echo "# FILEMARK START" > "$scriptPath"
-    echo "#!$(which zsh)" >> "$scriptPath"
-    echo "alias filemark=\"python3 \\\"$pythonScriptPath\\\" \\\"\$@\\\"\"" >> "$scriptPath"
+    echo "alias filemark=\"DATABASE_PATH=\\\"$DATABASE_PATH\\\" python3 \\\"$pythonScriptPath\\\" \\\"\$@\\\"\"" >> "$scriptPath"
     echo "# FILEMARK END" >> "$scriptPath"
-elif [[ $curr_shell == *"fish"* ]]; then
-    # Fish
-    profilePath="$HOME/.config/fish/config.fish"
-    echo "# FILEMARK START" > "$scriptPath"
-    echo "function filemark" >> "$scriptPath"
-    echo "    python3 \"$pythonScriptPath\" \$argv" >> "$scriptPath"
-    echo "# FILEMARK END" >> "$scriptPath"
+# elif [[ $curr_shell == *"fish"* ]]; then
+#     # Fish
+#     profilePath="$HOME/.config/fish/config.fish"
+#     echo "# FILEMARK START" > "$scriptPath"
+#     echo "function filemark" >> "$scriptPath"
+#     echo "    DATABASE_PATH=\\\"$DATABASE_PATH\\\" python3 \"$pythonScriptPath\" \$argv" >> "$scriptPath"
+#     echo "# FILEMARK END" >> "$scriptPath"
 else
     echo "Error: Unsupported shell. Please configure 'filemark' alias manually."
     exit 1
