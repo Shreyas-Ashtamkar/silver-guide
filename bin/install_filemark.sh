09f09c6e53b387d65ee2e8eb10b66706f4e02c33
Shreyas-Ashtamkar <shreyu@programmer.net>
2024-01-26 13:32:14 +0530

    Fix : Linux InstallScript

--- bin/install_filemark.sh
+++ bin/install_filemark.sh
@@ -4,20 +4,45 @@
 pythonScriptPath="$PWD/../filemark.py"
 scriptPath="./filemark.sh"
 
+# Define function to uninstall
+uninstall() {
+    echo "Uninstalling..."
+    rm -f "$scriptPath" # Remove the script file
+    sed -i '/# FILEMARK START/,/# FILEMARK END/d' "$profilePath" # Remove the added lines from the profile file
+    echo "Uninstallation complete."
+    exit 0
+}
+
+# Check if --uninstall parameter is provided
+if [[ "$1" == "--uninstall" ]]; then
+    uninstall
+fi
+
+# Get the default shell of the user from /etc/passwd
+curr_shell="$(getent passwd "$USER" | cut -d: -f7)"
+
 # Check the current shell
-if [ -n "$BASH_VERSION" ]; then
+if [[ $curr_shell == *"bash"* ]]; then
     # Bash
     profilePath="$HOME/.bashrc"
-    echo "alias filemark=\"python3 \\\"$pythonScriptPath\\\" \\\"\$@\\\"\"" > "$scriptPath"
-elif [ -n "$ZSH_VERSION" ]; then
+    echo "# FILEMARK START" > "$scriptPath"
+    echo "#!$(which bash)" >> "$scriptPath"
+    echo "alias filemark=\"python3 \\\"$pythonScriptPath\\\" \\\"\$@\\\"\"" >> "$scriptPath"
+    echo "# FILEMARK END" >> "$scriptPath"
+elif [[ $curr_shell == *"zsh"* ]]; then
     # Zsh
     profilePath="$HOME/.zshrc"
-    echo "alias filemark=\"python3 \\\"$pythonScriptPath\\\" \\\"\$@\\\"\"" > "$scriptPath"
-elif command -v fish >/dev/null 2>&1; then
+    echo "# FILEMARK START" > "$scriptPath"
+    echo "#!$(which zsh)" >> "$scriptPath"
+    echo "alias filemark=\"python3 \\\"$pythonScriptPath\\\" \\\"\$@\\\"\"" >> "$scriptPath"
+    echo "# FILEMARK END" >> "$scriptPath"
+elif [[ $curr_shell == *"fish"* ]]; then
     # Fish
     profilePath="$HOME/.config/fish/config.fish"
-    echo "function filemark" > "$scriptPath"
+    echo "# FILEMARK START" > "$scriptPath"
+    echo "function filemark" >> "$scriptPath"
     echo "    python3 \"$pythonScriptPath\" \$argv" >> "$scriptPath"
+    echo "# FILEMARK END" >> "$scriptPath"
 else
     echo "Error: Unsupported shell. Please configure 'filemark' alias manually."
     exit 1
